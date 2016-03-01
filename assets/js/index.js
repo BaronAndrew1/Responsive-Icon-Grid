0d592bbbe1cc5568c725ba7f4a5b5d9657ec39ae
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-01 17:09:02 -0500

    first go

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+var hoverTarget = $('li');
+
+hoverTarget.on("mouseover", function() {
+    $(this).find($('.bar-blue')).removeClass('active');
+    $(this).find($('.bar-white')).addClass('active');
+    $(this).css("background-color","#47a3da")
+    $(this).find($('i')).css("color","white")
+    $(this).find($('h1')).css("color","white")
+
+  });
+hoverTarget.on("mouseout", function() {
+    $(this).find($('.bar-blue')).addClass('active');
+    $(this).find($('.bar-white')).removeClass('active');
+    $(this).css("background-color","white")
+    $(this).find($('i')).css("color","#47a3da")
+    $(this).find($('h1')).css("color","#47a3da")
+  });
